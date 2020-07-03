b829a43d476a2d0fffeceefa25481ff70d7cc72e
Foxy <thefoxy41@gmail.com>
2020-07-03 04:00:08 +0200

    Initial commit

---
+++ src/main/java/fr/foxyz/betterchat/BetterChat.java
@@ -0,0 +1,16 @@
+package fr.foxyz.betterchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BetterChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
